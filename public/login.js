bb3229ad97d0f7ee18fef3088edc0a12e38772c5
Lucas Pereira Esteves <lucaspesteves2017@gmail.com>
2024-09-08 12:41:20 -0300

    teste

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://sqpmnvfdauytmrrtdfoc.supabase.co' ;//import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxcG1udmZkYXV5dG1ycnRkZm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MzY3MTgsImV4cCI6MjA0MDExMjcxOH0.pB4ru-NZiTbDHyLHp3G4M3fIjwFbS1LPJdqZg6xrMx0'; //import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 document.getElementById('login').addEventListener('submit', async function(event) {
