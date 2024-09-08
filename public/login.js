b3d373a319f8beff4c80390f0959582d3c9230a5
Lucas Pereira Esteves <lucaspesteves2017@gmail.com>
2024-09-08 12:53:15 -0300

    new CDN

--- public/login.js
+++ public/login.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/dist/supabase.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabaseUrl = 'https://sqpmnvfdauytmrrtdfoc.supabase.co' ;//import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxcG1udmZkYXV5dG1ycnRkZm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ1MzY3MTgsImV4cCI6MjA0MDExMjcxOH0.pB4ru-NZiTbDHyLHp3G4M3fIjwFbS1LPJdqZg6xrMx0'; //import.meta.env.VITE_SUPABASE_ANON_KEY;
 
